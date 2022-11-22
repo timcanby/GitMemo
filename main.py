f22b19c5af78f8ce2078b55ba03e54a07207cc4c
timcanby <kangyinglee0501@gmail.com>
2022-11-23 03:21:37 +0900

    create a

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('create A')
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
